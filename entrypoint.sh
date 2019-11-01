ea0aeaa9250974581e0cee1aaf4a651147fd9954
Hillel Arnold <helrond@hotmail.com>
2019-11-01 16:56:58 -0400

    improving deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-# Apply database migrations
-echo "Apply database migrations"
-./wait-for-it.sh db:5432 -- python manage.py migrate
+
+./wait-for-it.sh db:5432 -- echo "Running entrypoint.sh"
+
+if [ ! -f manage.py ]; then
+  cd scorpio
+fi
+
+if [ ! -f scorpio/config.py ]; then
+    cp scorpio/config.py.example scorpio/config.py
+fi
+
+python manage.py migrate
 
 #Start server
 echo "Starting server"
-python manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8008
