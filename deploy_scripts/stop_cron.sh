6b5cc9710f17432063577a169e26975460afe98d
Hillel Arnold <helrond@hotmail.com>
2020-05-18 16:31:14 -0500

    debug and roll cron in cron changes

--- deploy_scripts/stop_cron.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# stop all running cron jobs
-sudo pkill crond
-
-# stop cron
-sudo systemctl stop crond
--- deploy_scripts/substitute_env.sh
+++ deploy_scripts/substitute_env.sh
@@ -2,7 +2,7 @@
 set -e
 
 for TEMPLATE in scorpio/config.py.deploy \
-  appspec.yml.deploy
+  appspec.yml.deploy \
   deploy_scripts/*.deploy
 do
   envsubst < "$TEMPLATE" > echo "$TEMPLATE" | sed -e 's/\(\.deploy\)*$//g'
