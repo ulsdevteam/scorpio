cfd3e2928fea95a9b4755942c4598dbf2e7f6b19
Hillel Arnold <helrond@hotmail.com>
2020-05-19 13:29:56 -0500

    add codes

--- indexer/cron.py
+++ indexer/cron.py
@@ -21,45 +21,55 @@ class BaseCron(CronJobBase):
 
 
 class IndexAll(BaseCron):
+    code = "fetcher.index_all"
     object_type = None
 
 
 class IndexAllClean(BaseCron):
+    code = "fetcher.index_all_clean"
     object_type = None
     clean = True
 
 
 class IndexAgents(BaseCron):
+    code = "fetcher.index_agents"
     object_type = "agent"
 
 
 class IndexAgentsClean(BaseCron):
+    code = "fetcher.index_agents_clean"
     object_type = "agent"
     clean = True
 
 
 class IndexCollections(BaseCron):
+    code = "fetcher.index_collections"
     object_type = "collection"
 
 
 class IndexCollectionsClean(BaseCron):
+    code = "fetcher.index_collections_clean"
     object_type = "collection"
     clean = True
 
 
 class IndexObjects(BaseCron):
+    code = "fetcher.index_objects"
     object_type = "object"
 
 
 class IndexObjectsClean(BaseCron):
+    code = "fetcher.index_objects_clean"
     object_type = "object"
     clean = True
 
 
 class IndexTerms(BaseCron):
+    code = "fetcher.index_terms"
     object_type = "term"
 
 
 class IndexTermsClean(BaseCron):
+    code = "fetcher.index_terms_clean"
     object_type = "term"
     clean = True
