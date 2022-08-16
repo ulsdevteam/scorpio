07a201af7d21b886f7a0e5ea804fede3aab74126
Clinton Graham <ctgraham@pitt.edu>
2022-08-16 17:59:59 -0400

    #170: set cron returns to recent string values

--- indexer/cron.py
+++ indexer/cron.py
@@ -5,6 +5,7 @@ from django_cron import CronJobBase, Schedule
 from .indexers import Indexer
 from .models import IndexRun
 
+import json
 
 class BaseCron(CronJobBase):
     RUN_EVERY_MINS = 0
@@ -12,7 +13,7 @@ class BaseCron(CronJobBase):
     clean = False
 
     def do(self):
-        result = False
+        result = ''
         start = datetime.now()
         action = "Full" if self.clean else "Incremental"
         object_type = self.object_type if self.object_type else "all"
@@ -21,11 +22,12 @@ class BaseCron(CronJobBase):
         try:
             indexed = Indexer().add(object_type=self.object_type, clean=self.clean)
         except Exception as e:
+            result = str(e)
             print(e)
         end = datetime.now()
         print("{} records indexed in {}".format(len(indexed), end - start))
-        print("{} index of {} records complete at {}\n".format(action, object_type, end))
-        result = True
+        result = "{} index of {} records complete at {}\n".format(action, object_type, end)
+        print(result)
         return result
 
 
@@ -92,7 +94,10 @@ class CleanUpCompleted(CronJobBase):
 
     def do(self):
         try:
-            return IndexRun.objects.filter(indexrunerror__isnull=True, status=IndexRun.FINISHED).delete()
+            retval = IndexRun.objects.filter(indexrunerror__isnull=True, status=IndexRun.FINISHED).delete()
+            result = json.dumps(retval)
         except Exception as e:
-            print("Error cleaning up completed IndexRun objects: {}".format(e))
-            return False
+            result = "Error cleaning up completed IndexRun objects: {}".format(e)
+            print(result)
+        return result
+
