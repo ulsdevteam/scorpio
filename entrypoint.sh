7b602fb285f4a6b5ac39faa535ceb9cb381ff9ee
Tyler Lendon <trl75@pitt.edu>
2024-03-05 09:11:18 -0500

    Standardized entrypoint.sh to match other versions

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,36 @@
 #!/bin/bash
 
-
-./wait-for-it.sh db:5432 -- echo "Running entrypoint.sh"
-
 if [ ! -f manage.py ]; then
   cd scorpio
 fi
 
 if [ ! -f scorpio/config.py ]; then
-    cp scorpio/config.py.example scorpio/config.py
+    if [[ -n $PROD ]]; then
+      envsubst < scorpio/config.py.deploy > scorpio/config.py
+    else
+      cp scorpio/config.py.example scorpio/config.py
+    fi
 fi
 
-python manage.py migrate
+if [[ -n $CRON ]]; then
+  cron -f -L 2
+else  
+  ./wait-for-it.sh $db:${SQL_PORT} -- echo "Running entrypoint.sh"
+  python manage.py migrate
 
-#Start server
-echo "Starting server"
-python manage.py runserver 0.0.0.0:${APPLICATION_PORT}
+  #Start server
+  echo "Starting server"
+
+  #Start server
+  if [[ -n $PROD ]]; then
+      # Collect static files
+      echo "Collecting static files"
+      python manage.py collectstatic
+
+      chmod 775 /var/www/html/scorpio/static
+      chown www-data:www-data /var/www/html/scorpio/static
+      apache2ctl -D FOREGROUND
+  else
+      python manage.py runserver 0.0.0.0:${APPLICATION_PORT}
+  fi
+fi
